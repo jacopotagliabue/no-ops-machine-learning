ebb52b042e15905a1e057f35e145166ac27c8577
jacopo tagliabue <jacopo.tagliabue@tooso.org>
2021-01-21 15:00:09 -0500

    first end-to-end version (local flow)

--- flow/training.py
+++ flow/training.py
@@ -1,6 +1,29 @@
-from metaflow import FlowSpec, step, retry,  IncludeFile, Parameter, resources, batch, S3
+"""
+
+This is a one-script MetaFlow project, showing how to go from a dataset file to an endpoint serving predictions through
+a repeatable, modular and scalable process (DAG-based). More importantly, all of this is achieved from a developer
+laptop setup, without explicitely deploying *any infrastructure*.
+
+The functions are not particularly terse, but we aim for explanatory power over economy: our challenge is to find a
+compromise between a toy-example far removed from anything real, and something over-complicated and too tailored to
+a specific use case. In this example you can see:
+
+* the use of static file, versioned by MetaFlow automatically;
+* the use of the "fanout" capability, allowing parallel execution of steps;
+* the use of the "batch" capability, in which local execution and cloud-enhanced computing co-exist seamlessly;
+* a GPU-backed deployment and prediction testing in 10 lines of code.
+
+"""
+
+from metaflow import FlowSpec, step, IncludeFile, batch, S3
 import time
+import numpy as np
 from io import StringIO
+import os
+from dotenv import load_dotenv
+from random import choice
+# load env vars
+load_dotenv(dotenv_path='.env', verbose=True)
 
 
 class RegressionModel(FlowSpec):
@@ -15,25 +38,146 @@ class RegressionModel(FlowSpec):
     @step
     def start(self):
         """
+
         Read data in, and parallelize model building with two params (in this case, dummy example with learning rate).
 
-        :return:
         """
+        self.start_time = time.time()
         # data is an array of lines from the text file containing the numbers
         raw_data = StringIO(self.data_file).readlines()
         print("Total of {} rows in the dataset!".format(len(raw_data)))
         # cast strings to float and prepare for training
         self.dataset = [[float(_) for _ in d.strip().split('\t')] for d in raw_data]
         print("Raw data: {}, cleaned data: {}".format(raw_data[0].strip(), self.dataset[0]))
+        # store dataset as train and test set
+        split_index = int(len(self.dataset) * 0.8)
+        self.train_dataset = self.dataset[:split_index]
+        self.test_dataset = self.dataset[split_index:]
+        print("Training data: {}, test data: {}".format(len(self.train_dataset), len(self.test_dataset)))
         # this is the only MetaFlow-specific part: based on a list of options (here, learning rates)
         # spin up N parallel process, passing the given option to the child process
-        learning_rates = [0.1, 0.2]
-        #self.next(self.train_model, foreach='sports')
+        self.learning_rates = [0.1, 0.2]
+        self.next(self.train_model, foreach='learning_rates')
+
+    # @batch(gpu=1, memory=80000)
+    @step
+    def train_model(self):
+        """
+
+        Train a dummy regression model (https://www.tensorflow.org/tutorials/keras/regression) to predict a value
+        with the simple y = alpha x + beta model.
+
+        To make sure our GPU setup is working we print some verbose logging (totally unnecessary)
+
+        """
+        # this is the CURRENT learning rate in the fan-out
+        # each copy of this step in the parallelization will have it's own value
+        self.learning_rate = self.input
+        # do some specific import
+        import tensorflow as tf
+        from tensorflow.keras import layers
+        import tarfile
+        # build the model
+        x_train = np.array([[_[0]] for _ in self.train_dataset])
+        y_train = np.array([_[1] for _ in self.train_dataset])
+        x_test = np.array([[_[0]] for _ in self.test_dataset])
+        y_test = np.array([_[1] for _ in self.test_dataset])
+        x_model = tf.keras.Sequential([
+            layers.Dense(input_shape=[1,], units=1)
+        ])
+        # print out models for debug
+        print(x_model.summary())
+        x_model.compile(
+            optimizer=tf.optimizers.Adam(learning_rate=self.learning_rate),
+            loss='mean_absolute_error')
+        history = x_model.fit(x_train, y_train,epochs=100, validation_split=0.2)
+        self.hist = history.history
+        # store loss for downstream tasks
+        self.results = x_model.evaluate(x_test, y_test)
+        print("Test set results: {}".format(self.results))
+        # save model: IMPORTANT: TF models need to have a version
+        # see: https://github.com/aws/sagemaker-python-sdk/issues/1484
+        FOLDER_NAME = "regression-model-{}".format(self.learning_rate)
+        MODEL_NAME = "{}/1".format(FOLDER_NAME)
+        TAR_NAME = 'model-{}.tar.gz'.format(self.learning_rate)
+        x_model.save(filepath=MODEL_NAME)
+        # zip keras folder to a single file
+        with tarfile.open(TAR_NAME, mode="w:gz") as _tar:
+            _tar.add(MODEL_NAME, recursive=True)
+        # metaflow nice s3 client needs a byte object for the put
+        with open(TAR_NAME, "rb") as in_file:
+            data = in_file.read()
+            with S3(s3root=os.getenv('S3_BUCKET_MODEL_PREFIX').format(FOLDER_NAME)) as s3:
+                s3.put(TAR_NAME, data)
+        # finally join with the others
+        self.next(self.join_runs)
+
+    @step
+    def join_runs(self, inputs):
+        """
+        Join the parallel runs and merge results into a dictionary.
+        """
+        # merge results (loss) from runs with different learning rates
+        self.results_from_runs = {inp.learning_rate: inp.results for inp in inputs}
+        print("Current results: {}".format(self.results_from_runs))
+        # pick one according to some rule, say smaller loss (here just pick a random one)
+        self.best_learning_rate = choice(list(self.results_from_runs.keys()))
+        # rebuild here the full s3 path to the target model file, as stored in S3
+        # note: this is repeated from above step, a better naming convention can be enforced here
+        # but just making it work for now
+        FOLDER_NAME = "regression-model-{}".format(self.best_learning_rate)
+        TAR_NAME = 'model-{}.tar.gz'.format(self.best_learning_rate)
+        self.best_s3_model_path = '{}/{}'.format(
+            os.getenv('S3_BUCKET_MODEL_PREFIX').format(FOLDER_NAME),
+            TAR_NAME)
+        # next, deploy
+        self.next(self.deploy)
+
+    @step
+    def deploy(self):
+        """
+        Use SageMaker to deploy the model as a stand-alone, PaaS endpoint, with our choice of the underlying
+        Docker image and hardware capabilities.
+
+        Available images for inferences can be chosen from AWS official list:
+        https://github.com/aws/deep-learning-containers/blob/master/available_images.md
+
+        Once the endpoint is deployed, you can add a further step with for example behavioral testing, to
+        ensure model robustness (e.g. see https://arxiv.org/pdf/2005.04118.pdf). Here, we just "prove" that
+        the endpoint is up and running!
+
+        """
+        from sagemaker.tensorflow import TensorFlowModel
+        # generate a signature for the endpoint, using learning rate and timestamp as a convention
+        ENDPOINT_NAME = 'regression-{}-endpoint'.format(int(round(time.time() * 1000)))
+        # print out the name, so that we can use it when deploying our lambda
+        print("\n\n================\nEndpoint name is: {}\n\n".format(ENDPOINT_NAME))
+        # this is the name of the IAM role with SageMaker permissions
+        # make sure this role has access to the bucket containing the model tar file!
+        IAM_SAGE_ROLE = 'MetaSageMakerRole'
+        model = TensorFlowModel(model_data=self.best_s3_model_path,
+                               image_uri='763104351884.dkr.ecr.us-west-2.amazonaws.com/tensorflow-inference:2.3.0-gpu-py37-cu102-ubuntu18.04',
+                               role=IAM_SAGE_ROLE)
+        predictor = model.deploy(initial_instance_count=1,
+                                instance_type='ml.p3.2xlarge',
+                                endpoint_name=ENDPOINT_NAME)
+        # run a small test against the endpoint
+        # pick a number for X and check the predicted Y is sensible
+        input = {'instances': np.array([[0.57457947234]])}
+        result = predictor.predict(input)
+        print(input, result)
+        assert result > 0
         self.next(self.end)
 
     @step
     def end(self):
-        print('End')
+        """
+        The final step is empty here, but cleaning operations and/or sending hooks for downstream deployment tasks
+        is a natural necessity for machine learning DAGs.
+
+        """
+        self.dag_duration = time.time() - self.start_time
+        print('Dag ended after {} seconds.'.format(self.dag_duration))
 
 
 if __name__ == '__main__':
